c5a184c1c9f53e735deb3a90f9dc63836f7e9ab9
ShrutiR17 <shruti.rajasekar@gmail.com>
2022-12-25 13:10:26 +0530

    mapfunction

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+//import './App.css';
+import PlanetList from './PlanetList'
+function App() {
+  return (
+    <div className="App">
+      <PlanetList />
+    </div>
+  );
+}
+export default App;
